689ef7e65fa41721475909e50d4f9e53171001d8
kindunq <humanb2box@gmail.com>
2019-02-25 17:34:09 +0900

    docs: apolloprovider, client, style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+import { ApolloProvider} from "react-apollo";
+import client from "./apollo";
+import GlobalStyle from "./globalStyles";
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ApolloProvider client={client} > <GlobalStyle /><App /></ApolloProvider>, document.getElementById('root'));
 
